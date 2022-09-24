8950813254be5b459a5a1b8ea5b12e1328b6cfd6
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-09-24 11:28:31 +0900

    Session がラップしているので ServiceContent を private に変更した。

--- CsVmomi/Session.cs
+++ CsVmomi/Session.cs
@@ -9,115 +9,115 @@
 
     public class Session
     {
+        private readonly ServiceContent serviceContent;
+
         private Session(IClient client, ServiceContent serviceContent)
         {
             this.Client = client;
-            this.ServiceContent = serviceContent;
+            this.serviceContent = serviceContent;
         }
 
-        public AboutInfo About => this.ServiceContent.about;
+        public AboutInfo About => this.serviceContent.about;
 
-        public HostLocalAccountManager AccountManager => ManagedObject.Create<HostLocalAccountManager>(this.ServiceContent.accountManager, this);
+        public HostLocalAccountManager AccountManager => ManagedObject.Create<HostLocalAccountManager>(this.serviceContent.accountManager, this);
 
-        public AlarmManager AlarmManager => ManagedObject.Create<AlarmManager>(this.ServiceContent.alarmManager, this);
+        public AlarmManager AlarmManager => ManagedObject.Create<AlarmManager>(this.serviceContent.alarmManager, this);
 
-        public AuthorizationManager AuthorizationManager => ManagedObject.Create<AuthorizationManager>(this.ServiceContent.authorizationManager, this);
+        public AuthorizationManager AuthorizationManager => ManagedObject.Create<AuthorizationManager>(this.serviceContent.authorizationManager, this);
 
-        public CertificateManager CertificateManager => ManagedObject.Create<CertificateManager>(this.ServiceContent.certificateManager, this);
+        public CertificateManager CertificateManager => ManagedObject.Create<CertificateManager>(this.serviceContent.certificateManager, this);
 
-        public ClusterProfileManager ClusterProfileManager => ManagedObject.Create<ClusterProfileManager>(this.ServiceContent.clusterProfileManager, this);
+        public ClusterProfileManager ClusterProfileManager => ManagedObject.Create<ClusterProfileManager>(this.serviceContent.clusterProfileManager, this);
 
-        public ProfileComplianceManager ComplianceManager => ManagedObject.Create<ProfileComplianceManager>(this.ServiceContent.complianceManager, this);
+        public ProfileComplianceManager ComplianceManager => ManagedObject.Create<ProfileComplianceManager>(this.serviceContent.complianceManager, this);
 
-        public CryptoManager CryptoManager => ManagedObject.Create<CryptoManager>(this.ServiceContent.cryptoManager, this);
+        public CryptoManager CryptoManager => ManagedObject.Create<CryptoManager>(this.serviceContent.cryptoManager, this);
 
-        public CustomFieldsManager CustomFieldsManager => ManagedObject.Create<CustomFieldsManager>(this.ServiceContent.customFieldsManager, this);
+        public CustomFieldsManager CustomFieldsManager => ManagedObject.Create<CustomFieldsManager>(this.serviceContent.customFieldsManager, this);
 
-        public CustomizationSpecManager CustomizationSpecManager => ManagedObject.Create<CustomizationSpecManager>(this.ServiceContent.customizationSpecManager, this);
+        public CustomizationSpecManager CustomizationSpecManager => ManagedObject.Create<CustomizationSpecManager>(this.serviceContent.customizationSpecManager, this);
 
-        public DatastoreNamespaceManager DatastoreNamespaceManager => ManagedObject.Create<DatastoreNamespaceManager>(this.ServiceContent.datastoreNamespaceManager, this);
+        public DatastoreNamespaceManager DatastoreNamespaceManager => ManagedObject.Create<DatastoreNamespaceManager>(this.serviceContent.datastoreNamespaceManager, this);
 
-        public DiagnosticManager DiagnosticManager => ManagedObject.Create<DiagnosticManager>(this.ServiceContent.diagnosticManager, this);
+        public DiagnosticManager DiagnosticManager => ManagedObject.Create<DiagnosticManager>(this.serviceContent.diagnosticManager, this);
 
-        public DistributedVirtualSwitchManager DvSwitchManager => ManagedObject.Create<DistributedVirtualSwitchManager>(this.ServiceContent.dvSwitchManager, this);
+        public DistributedVirtualSwitchManager DvSwitchManager => ManagedObject.Create<DistributedVirtualSwitchManager>(this.serviceContent.dvSwitchManager, this);
 
-        public EventManager EventManager => ManagedObject.Create<EventManager>(this.ServiceContent.eventManager, this);
+        public EventManager EventManager => ManagedObject.Create<EventManager>(this.serviceContent.eventManager, this);
 
-        public ExtensionManager ExtensionManager => ManagedObject.Create<ExtensionManager>(this.ServiceContent.extensionManager, this);
+        public ExtensionManager ExtensionManager => ManagedObject.Create<ExtensionManager>(this.serviceContent.extensionManager, this);
 
-        public FailoverClusterConfigurator FailoverClusterConfigurator => ManagedObject.Create<FailoverClusterConfigurator>(this.ServiceContent.failoverClusterConfigurator, this);
+        public FailoverClusterConfigurator FailoverClusterConfigurator => ManagedObject.Create<FailoverClusterConfigurator>(this.serviceContent.failoverClusterConfigurator, this);
 
-        public FailoverClusterManager FailoverClusterManager => ManagedObject.Create<FailoverClusterManager>(this.ServiceContent.failoverClusterManager, this);
+        public FailoverClusterManager FailoverClusterManager => ManagedObject.Create<FailoverClusterManager>(this.serviceContent.failoverClusterManager, this);
 
-        public FileManager FileManager => ManagedObject.Create<FileManager>(this.ServiceContent.fileManager, this);
+        public FileManager FileManager => ManagedObject.Create<FileManager>(this.serviceContent.fileManager, this);
 
-        public VirtualMachineGuestCustomizationManager GuestCustomizationManager => ManagedObject.Create<VirtualMachineGuestCustomizationManager>(this.ServiceContent.guestCustomizationManager, this);
+        public VirtualMachineGuestCustomizationManager GuestCustomizationManager => ManagedObject.Create<VirtualMachineGuestCustomizationManager>(this.serviceContent.guestCustomizationManager, this);
 
-        public GuestOperationsManager GuestOperationsManager => ManagedObject.Create<GuestOperationsManager>(this.ServiceContent.guestOperationsManager, this);
+        public GuestOperationsManager GuestOperationsManager => ManagedObject.Create<GuestOperationsManager>(this.serviceContent.guestOperationsManager, this);
 
-        public HealthUpdateManager HealthUpdateManager => ManagedObject.Create<HealthUpdateManager>(this.ServiceContent.healthUpdateManager, this);
+        public HealthUpdateManager HealthUpdateManager => ManagedObject.Create<HealthUpdateManager>(this.serviceContent.healthUpdateManager, this);
 
-        public HostProfileManager HostProfileManager => ManagedObject.Create<HostProfileManager>(this.ServiceContent.hostProfileManager, this);
+        public HostProfileManager HostProfileManager => ManagedObject.Create<HostProfileManager>(this.serviceContent.hostProfileManager, this);
 
-        public HostSpecificationManager HostSpecManager => ManagedObject.Create<HostSpecificationManager>(this.ServiceContent.hostSpecManager, this);
+        public HostSpecificationManager HostSpecManager => ManagedObject.Create<HostSpecificationManager>(this.serviceContent.hostSpecManager, this);
 
-        public IoFilterManager IoFilterManager => ManagedObject.Create<IoFilterManager>(this.ServiceContent.ioFilterManager, this);
+        public IoFilterManager IoFilterManager => ManagedObject.Create<IoFilterManager>(this.serviceContent.ioFilterManager, this);
 
-        public IpPoolManager IpPoolManager => ManagedObject.Create<IpPoolManager>(this.ServiceContent.ipPoolManager, this);
+        public IpPoolManager IpPoolManager => ManagedObject.Create<IpPoolManager>(this.serviceContent.ipPoolManager, this);
 
-        public LicenseManager LicenseManager => ManagedObject.Create<LicenseManager>(this.ServiceContent.licenseManager, this);
+        public LicenseManager LicenseManager => ManagedObject.Create<LicenseManager>(this.serviceContent.licenseManager, this);
 
-        public LocalizationManager LocalizationManager => ManagedObject.Create<LocalizationManager>(this.ServiceContent.localizationManager, this);
+        public LocalizationManager LocalizationManager => ManagedObject.Create<LocalizationManager>(this.serviceContent.localizationManager, this);
 
-        public OverheadMemoryManager OverheadMemoryManager => ManagedObject.Create<OverheadMemoryManager>(this.ServiceContent.overheadMemoryManager, this);
+        public OverheadMemoryManager OverheadMemoryManager => ManagedObject.Create<OverheadMemoryManager>(this.serviceContent.overheadMemoryManager, this);
 
-        public OvfManager OvfManager => ManagedObject.Create<OvfManager>(this.ServiceContent.ovfManager, this);
+        public OvfManager OvfManager => ManagedObject.Create<OvfManager>(this.serviceContent.ovfManager, this);
 
-        public PerformanceManager PerfManager => ManagedObject.Create<PerformanceManager>(this.ServiceContent.perfManager, this);
+        public PerformanceManager PerfManager => ManagedObject.Create<PerformanceManager>(this.serviceContent.perfManager, this);
 
-        public PropertyCollector PropertyCollector => ManagedObject.Create<PropertyCollector>(this.ServiceContent.propertyCollector, this);
+        public PropertyCollector PropertyCollector => ManagedObject.Create<PropertyCollector>(this.serviceContent.propertyCollector, this);
 
-        public Folder RootFolder => ManagedObject.Create<Folder>(this.ServiceContent.rootFolder, this);
+        public Folder RootFolder => ManagedObject.Create<Folder>(this.serviceContent.rootFolder, this);
 
-        public ScheduledTaskManager ScheduledTaskManager => ManagedObject.Create<ScheduledTaskManager>(this.ServiceContent.scheduledTaskManager, this);
+        public ScheduledTaskManager ScheduledTaskManager => ManagedObject.Create<ScheduledTaskManager>(this.serviceContent.scheduledTaskManager, this);
 
-        public SearchIndex SearchIndex => ManagedObject.Create<SearchIndex>(this.ServiceContent.searchIndex, this);
+        public SearchIndex SearchIndex => ManagedObject.Create<SearchIndex>(this.serviceContent.searchIndex, this);
 
-        public ServiceManager ServiceManager => ManagedObject.Create<ServiceManager>(this.ServiceContent.serviceManager, this);
+        public ServiceManager ServiceManager => ManagedObject.Create<ServiceManager>(this.serviceContent.serviceManager, this);
 
-        public SessionManager SessionManager => ManagedObject.Create<SessionManager>(this.ServiceContent.sessionManager, this);
+        public SessionManager SessionManager => ManagedObject.Create<SessionManager>(this.serviceContent.sessionManager, this);
 
-        public OptionManager Setting => ManagedObject.Create<OptionManager>(this.ServiceContent.setting, this);
+        public OptionManager Setting => ManagedObject.Create<OptionManager>(this.serviceContent.setting, this);
 
-        public SiteInfoManager SiteInfoManager => ManagedObject.Create<SiteInfoManager>(this.ServiceContent.siteInfoManager, this);
+        public SiteInfoManager SiteInfoManager => ManagedObject.Create<SiteInfoManager>(this.serviceContent.siteInfoManager, this);
 
-        public HostSnmpSystem SnmpSystem => ManagedObject.Create<HostSnmpSystem>(this.ServiceContent.snmpSystem, this);
+        public HostSnmpSystem SnmpSystem => ManagedObject.Create<HostSnmpSystem>(this.serviceContent.snmpSystem, this);
 
-        public StorageQueryManager StorageQueryManager => ManagedObject.Create<StorageQueryManager>(this.ServiceContent.storageQueryManager, this);
+        public StorageQueryManager StorageQueryManager => ManagedObject.Create<StorageQueryManager>(this.serviceContent.storageQueryManager, this);
 
-        public StorageResourceManager StorageResourceManager => ManagedObject.Create<StorageResourceManager>(this.ServiceContent.storageResourceManager, this);
+        public StorageResourceManager StorageResourceManager => ManagedObject.Create<StorageResourceManager>(this.serviceContent.storageResourceManager, this);
 
-        public TaskManager TaskManager => ManagedObject.Create<TaskManager>(this.ServiceContent.taskManager, this);
+        public TaskManager TaskManager => ManagedObject.Create<TaskManager>(this.serviceContent.taskManager, this);
 
-        public TenantTenantManager TenantManager => ManagedObject.Create<TenantTenantManager>(this.ServiceContent.tenantManager, this);
+        public TenantTenantManager TenantManager => ManagedObject.Create<TenantTenantManager>(this.serviceContent.tenantManager, this);
 
-        public UserDirectory UserDirectory => ManagedObject.Create<UserDirectory>(this.ServiceContent.userDirectory, this);
+        public UserDirectory UserDirectory => ManagedObject.Create<UserDirectory>(this.serviceContent.userDirectory, this);
 
-        public ViewManager ViewManager => ManagedObject.Create<ViewManager>(this.ServiceContent.viewManager, this);
+        public ViewManager ViewManager => ManagedObject.Create<ViewManager>(this.serviceContent.viewManager, this);
 
-        public VirtualDiskManager VirtualDiskManager => ManagedObject.Create<VirtualDiskManager>(this.ServiceContent.virtualDiskManager, this);
+        public VirtualDiskManager VirtualDiskManager => ManagedObject.Create<VirtualDiskManager>(this.serviceContent.virtualDiskManager, this);
 
-        public VirtualizationManager VirtualizationManager => ManagedObject.Create<VirtualizationManager>(this.ServiceContent.virtualizationManager, this);
+        public VirtualizationManager VirtualizationManager => ManagedObject.Create<VirtualizationManager>(this.serviceContent.virtualizationManager, this);
 
-        public VirtualMachineCompatibilityChecker VmCompatibilityChecker => ManagedObject.Create<VirtualMachineCompatibilityChecker>(this.ServiceContent.vmCompatibilityChecker, this);
+        public VirtualMachineCompatibilityChecker VmCompatibilityChecker => ManagedObject.Create<VirtualMachineCompatibilityChecker>(this.serviceContent.vmCompatibilityChecker, this);
 
-        public VirtualMachineProvisioningChecker VmProvisioningChecker => ManagedObject.Create<VirtualMachineProvisioningChecker>(this.ServiceContent.vmProvisioningChecker, this);
+        public VirtualMachineProvisioningChecker VmProvisioningChecker => ManagedObject.Create<VirtualMachineProvisioningChecker>(this.serviceContent.vmProvisioningChecker, this);
 
-        public VStorageObjectManagerBase VStorageObjectManager => ManagedObject.Create<VStorageObjectManagerBase>(this.ServiceContent.vStorageObjectManager, this);
+        public VStorageObjectManagerBase VStorageObjectManager => ManagedObject.Create<VStorageObjectManagerBase>(this.serviceContent.vStorageObjectManager, this);
 
         internal IClient Client { get; }
-
-        internal ServiceContent ServiceContent { get; }
 
         public static async System.Threading.Tasks.Task<Session> Get(Uri url)
         {
